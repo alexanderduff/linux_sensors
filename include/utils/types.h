afe171fd74772851fc2f436c5e8c53673f478beb
alex <amd435@drexel.edu>
2016-02-05 17:29:58 -0500

    Pushing to package the program for the team.

--- include/utils/types.h
+++ include/utils/types.h
@@ -5,5 +5,6 @@
 #ifndef HIGHPERFORMANCELINUXSENSORS_TYPES_H
 #define HIGHPERFORMANCELINUXSENSORS_TYPES_H
 typedef int pid_t;
-typedef int syscall_t;
+typedef unsigned long int syscall_t;
+//typedef int syscall_t;
 #endif //HIGHPERFORMANCELINUXSENSORS_TYPES_H
