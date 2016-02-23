fd073ab7d3f8d79347b1a8d8ad1333e6ed498aa2
alex <amd435@drexel.edu>
2016-02-23 10:49:39 -0500

    Fixed the negative numbers on syscalls issue.  Thanks to Bander!

--- include/utils/types.h
+++ include/utils/types.h
@@ -4,7 +4,7 @@
 
 #ifndef HIGHPERFORMANCELINUXSENSORS_TYPES_H
 #define HIGHPERFORMANCELINUXSENSORS_TYPES_H
-typedef short pid_t;
-typedef short syscall_t;
+typedef int pid_t;
+typedef int syscall_t;
 //typedef int syscall_t;
 #endif //HIGHPERFORMANCELINUXSENSORS_TYPES_H
